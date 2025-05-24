c81a03c0e4845c8dba246729d449d7468f2740c3
thalyson <thalysonlima820@gmail.com>
2025-05-24 11:04:57 -0300

    hjk

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <div className='logo'>
         <img src={logo} alt="logo" />
       </div>
-      <a href="https://api.whatsapp.com/send/?phone=5591984916040&text=Ol%C3%A1%2C+quero+mais+informa%C3%A7%C3%B5es%21&type=phone_number&app_absent=0" className="whatsapp-float" target="_blank" rel="noopener noreferrer">
+      <a href="https://api.whatsapp.com/send/?phone=5591984916040&text=Ol%C3%A1%2C+quero+mais+informa%C3%A7%C3%B5es%21%sobre%21%energia%21%Igreen%21%&type=phone_number&app_absent=0" className="whatsapp-float" target="_blank" rel="noopener noreferrer">
         <img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt="WhatsApp" />
       </a>
 
